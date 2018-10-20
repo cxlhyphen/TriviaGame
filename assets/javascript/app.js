a41ca93090876e707378c58060030f9ab9209a0c
cxlhyphen <42073246+cxlhyphen@users.noreply.github.com>
2018-10-20 19:02:28 -0400

    more progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,12 @@ $("#start").on("click", function () {
     game.loadQ();
 });
 
-$(document).on("click", '.buttonA', function(event) {
+$(document).on("click", ".buttonA", function(event) {
     game.clicked(event);
+});
+
+$(document).on("click", "#reset", function() {
+    game.reset();
 })
 
 var questions = [
@@ -12,7 +16,7 @@ var questions = [
         question: "Which country consumes the most coffee per capita?",
         answers: ["Finland", "Belgium", "Italy", "United States"],
         correctAnswer: "Finland",
-        image: "A"
+        image: "../images.joakim-hokasalo-finflag.jpg"
     },
     {
         question: "How much coffee does the average American coffee drinker consume in a day? (1 cup = 8 oz.)",
@@ -52,29 +56,46 @@ var game = {
         game.counter--;
         $("#counter").text(game.counter);
         if (game.counter == 0) {
-            alert("TIME UP");
             game.timeUp();
         }
     },
 
     loadQ: function() {
         timer = setInterval(game.countdown, 1000);
-        $("#display").html("<h2>" + questions[game.currQuestion].question + "</h2>");
+        $("#display").html('<p>Time left: <span id="counter">20</span> seconds</p>')
+        $("#display").append("<h2>" + questions[game.currQuestion].question + "</h2>");
         for (var i = 0; i < questions[game.currQuestion].answers.length; i++) {
             $("#display").append('<button class="buttonA" id="button-' + i + '" data-name="' + questions[game.currQuestion].answers[i] + '">' + questions[game.currQuestion].answers[i] + '</button>');
         }
     },
 
     nextQ: function() {
-
+        game.counter = 20;
+        $("#counter").text(game.counter);
+        game.currQuestion++;
+        game.loadQ();
     },
 
     timeUp: function() {
+        clearInterval(timer);
+        game.unanswered++;
+        $("#display").html("<h2>Time&rsquo;s Up!</h2>");
+        $("#display").append("<p>The correct answer is: " + questions[game.currQuestion].correctAnswer + "</p>");
 
+        if(game.currQuestion == questions.length - 1) {
+            setTimeout(game.results, 2000);
+        } else {
+            setTimeout(game.nextQ, 2000)
+        }
     },
 
     results: function() {
-
+        clearInterval(timer);
+        $("#display").html("<h2>You&rsquo;ve completed the game, here are your results:</h2>");
+        $("#display").append("<p>Correct: " + game.correct + "</p>");
+        $("#display").append("<p>Incorrect: " + game.incorrect + "</p>");
+        $("#display").append("<p>Unanswered: " + game.unanswered + "</p>");
+        $("#display").append('<button id="reset">Play again</button>');
     },
 
     clicked: function(event) {
@@ -87,20 +108,39 @@ var game = {
     },
 
     correctA: function() {
-        console.log("correct!");
         clearInterval(timer);
         game.correct++;
+        $("#display").html("<h2>Correct!</h2>");
+        $("#display").append(questions[game.currQuestion].image);
+
+        if(game.currQuestion == questions.length - 1) {
+            setTimeout(game.results, 2000);
+        } else {
+            setTimeout(game.nextQ, 2000)
+        }
         
     },
 
     incorrectA: function() {
-        console.log("incorrect!");
         clearInterval(timer);
         game.incorrect++;
+        $("#display").html("<h2>Incorrect</h2>");
+        $("#display").append("<p>The correct answer is: " + questions[game.currQuestion].correctAnswer + "</p>");
+
+        if(game.currQuestion == questions.length - 1) {
+            setTimeout(game.results, 2000);
+        } else {
+            setTimeout(game.nextQ, 2000)
+        }
     },
 
     reset: function() {
-
+        game.currQuestion = 0;
+        game.counter = 0;
+        game.correct = 0;
+        game.incorrect = 0;
+        game.unanswered = 0;
+        game.loadQ();
     }
 
 }
